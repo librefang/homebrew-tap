a2ec89678cad90cddc02bea6236747d7c742c8ac
Evan Hu <e-hu@unext.jp>
2026-03-12 19:56:34 +0900

    Update formula

--- Formula/librefang.rb
+++ Formula/librefang.rb
@@ -1,27 +1,18 @@
 class Librefang < Formula
-  desc "Community-Maintained Agent Operating System"
+  desc "Community-Maintained Agent Operating System written in Rust"
   homepage "https://librefang.ai"
-  url "https://github.com/librefang/librefang.git"
-  version "0.3.48"
   license "MIT"
+  version "0.3.48"
 
-  bottle do
-    rebuild 1
-    sha256 arm64_sequoia: "REPLACE_WITH_ACTUAL_SHA256"
-    sha256 x86_64_sequoia: "REPLACE_WITH_ACTUAL_SHA256"
-    sha256 arm64_monterey: "REPLACE_WITH_ACTUAL_SHA256"
-    sha256 x86_64_monterey: "REPLACE_WITH_ACTUAL_SHA256"
-  end
-
-  option "with-desktop", "Build with Tauri desktop app support"
-
-  if OS.mac?
+  on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/librefang/librefang/releases/download/v0.3.48/librefang-aarch64-apple-darwin.tar.gz"
     else
       url "https://github.com/librefang/librefang/releases/download/v0.3.48/librefang-x86_64-apple-darwin.tar.gz"
     end
-  elsif OS.linux?
+  end
+
+  on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/librefang/librefang/releases/download/v0.3.48/librefang-aarch64-unknown-linux-gnu.tar.gz"
     else
